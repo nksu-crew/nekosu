9fc815a3c7c395934ec42635174ab214fbdc508c
aqnya <xiaweilan56@gmail.com>
2026-05-21 17:29:04 +0800

    kernel: opz code.

--- src/syscall/type.h
+++ src/syscall/type.h
@@ -1,20 +1,4 @@
 #pragma once
 #include <linux/types.h>
 
-struct nksu_args {
-    struct pt_regs *regs;
-    __u32 cmd;
-    __u32 nr;
-    __u64 arg0;
-    __u64 arg1;
-    __u64 arg2;
-    __u64 arg3;
-    __u64 arg4;
-    __u64 arg5;
-};
-
 typedef long (*nksu_handler_t)(struct pt_regs *regs);
-
-#define NKSU_CMD_PING           0
-#define NKSU_CMD_CHECK_UID      1
-#define NKSU_CMD_SYSCALL_CALL   0xFF
