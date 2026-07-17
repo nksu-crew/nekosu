475766a78a0f9f5b9125dc1d9c49de08432cb05c
四月 <153448995+aqnya@users.noreply.github.com>
2026-07-17 09:31:15 +0800

    Delete userspace/libncore directory

--- userspace/libncore/ctl/ioctl.c
+++
@@ -1,4 +0,0 @@
-#include <linux/ioctl.h>
-#include <sys/ioctl.h>
-
-int bioctl(int fd, unsigned long cmd, void *arg) { return ioctl(fd, cmd, arg); }
--- userspace/libncore/go.mod
+++
@@ -1,5 +0,0 @@
-module nekosu/libncore
-
-go 1.26.2
-
-require golang.org/x/sys v0.43.0
